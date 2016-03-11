a44b2ab2407f87b6539cab9f81634b41976a7686
felipe <felipe@localhost.localdomain>
2016-03-10 22:11:54 -0600

    Initial commit, insertion form with different types

---
+++ common.js
@@ -0,0 +1,106 @@
+Data = new Mongo.Collection('data');
+
+var Schemas = {};
+
+Schemas.Person = new SimpleSchema({
+  firstName: {
+    type: String,
+    label: "Name"
+    },
+  lastName: {
+    type: String,
+    label: "Last Name"
+    },
+  age: {
+    type: Number,
+    label: "Age",
+    optional: true
+    },
+  password: {
+    type: String,
+    autoform: {
+      afFieldInput: {
+        type: "password"
+      }
+    }
+  },
+  birthDate: {
+    type: Date,
+    label: "Birth Date (YYYY-MM-DD)",
+    autoform: {
+      afFieldInput: {
+        type: "date"
+      }
+    } 
+  },
+  gender: {
+    type: String,
+    // allowedValues: ["Male", "Female"],
+    autoform: {
+      afFieldInput: {
+        firstOption: "(Not Specified)",
+        options: function() {
+          return [
+            {label: "Male", value: "Male"},
+            {label: "Female", value: "Female"}
+          ]
+        }
+      }
+    }
+  },
+  phones: {
+    type: Object,
+    optional: true
+  },
+  'phones.home': {
+     type: Number,
+     optional: true
+  },
+  'phones.work': {
+     type: Number,
+     optional: true
+  },
+  'phones.cell': {
+     type: Number,
+     optional: true
+  },
+  transportation: {
+    type: [String],
+    optional: true,
+    label: "Please specify your transportation means",
+    autoform: {
+      afFieldInput: {
+        type: "select-checkbox",
+        options: function() {
+          return [
+            {label: "Automobile", value: "Automobile"},
+            {label: "Motorcycle", value: "Motorcycle"},
+            {label: "Bicycle", value: "Bicycle"},
+            {label: "On Foot", value: "onfoot"}
+          ]
+        }
+      }
+    }
+  },
+  registeredVoter: {
+    type: Boolean,
+    label: "I am a registered voter",
+    autoform: {
+      afFieldInput: {
+        type: "boolean-checkbox",
+      }
+    }
+  },      
+  comment: {
+    type: String,
+    label: "Tell us about yourself",
+    optional: true,
+    autoform: {
+      afFieldInput: {
+        type: "textarea"
+      }
+    }
+  }
+})
+
+Data.attachSchema(Schemas.Person);
